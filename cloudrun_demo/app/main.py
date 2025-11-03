8b17ce2cd8f5348baa125243f03f0e68028b7bf8
Angus Tu <angus.tu@mile.cloud>
2025-11-03 11:12:25 +0800

    add: cloud run

--- cloudrun_demo/app/main.py
+++ cloudrun_demo/app/main.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def hello():
-    return jsonify({"message": "Hello, Cloud Run!"})
+    return {"message": "Hello, Cloud Run!"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))  
+    app.run(host="0.0.0.0", port=port)        
