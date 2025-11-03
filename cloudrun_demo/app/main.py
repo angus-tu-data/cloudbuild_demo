b0c87f04b7d112ad1dade2b1f266c4f2e8ebb10a
Angus Tu <angus.tu@mile.cloud>
2025-11-03 11:08:18 +0800

    add: cloud run

--- cloudrun_demo/app/main.py
+++ cloudrun_demo/app/main.py
@@ -7,4 +7,5 @@ def hello():
     return jsonify({"message": "Hello, Cloud Run!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
